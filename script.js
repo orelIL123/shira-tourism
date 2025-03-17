193c95d04ea6eb6591eeac7e10318b6f10cbec75
orelIL123 <orel895@gmail.com>
2025-03-17 18:33:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -376,3 +376,12 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     }
 }); 
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    });
+});
